58b9af90c02335d555dcdb16c29b915d5dde5b6c
YassineDev01 <yassinesimplon@gmail.com>
2026-01-06 11:59:18 +0100

    Premier commit

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -11,8 +11,20 @@ final class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('pages/index.html.twig', [
+            'controller_name' => 'pagesController',
         ]);
+    }
+
+    #[Route('/a-propos', name: 'app_apropos')]
+    public function apropos(): Response
+    {
+        return $this->render('pages/apropos.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig');
     }
 }
